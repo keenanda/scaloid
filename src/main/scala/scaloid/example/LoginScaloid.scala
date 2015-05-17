bfeba3a743df0d053add083e48e4bf1b053a5442
Dave Keenan <davek@spindance.com>
2015-05-17 12:06:32 -0400

    add progress dialog

--- src/main/scala/scaloid/example/LoginScaloid.scala
+++ src/main/scala/scaloid/example/LoginScaloid.scala
@@ -1,8 +1,19 @@
 package scaloid.example
 
+import java.util.concurrent.{ThreadPoolExecutor, LinkedBlockingQueue, TimeUnit}
+
+import android.app.ProgressDialog
+import android.os.AsyncTask
 import android.view.Gravity
 import android.widget.ImageView.ScaleType
 import org.scaloid.common._
+import scala.concurrent._
+
+object AndroidExecutionContext {
+  implicit val exec = ExecutionContext.fromExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+}
+
+import AndroidExecutionContext._
 
 class LoginScaloid extends SActivity {
 
@@ -13,7 +24,7 @@ class LoginScaloid extends SActivity {
         SImageView().<<.wrap.marginBottom(40 dip).marginTop(40 dip).Gravity(Gravity.CENTER_HORIZONTAL).>>.setImageResource(R.drawable.logo)
         val mEmail = SEditText().<<.margin(10 dip).>>.inputType(TEXT_EMAIL_ADDRESS).hint(R.string.email)
         val mPassword = SEditText().<<.margin(10 dip).>>.inputType(TEXT_PASSWORD).hint(R.string.password)
-        SButton(R.string.login, signin(mEmail.text.toString, mPassword.text.toString))
+        SButton(R.string.login, loginPressed(mEmail.text.toString, mPassword.text.toString))
           .<<.wrap.marginRight(40 dip).marginLeft(40 dip).Gravity(Gravity.CENTER_HORIZONTAL).>>.padding(30 dip, 0, 30 dip, 0).background(R.drawable.bg_selector)
       }.<<.Gravity(Gravity.CENTER).>>.padding(30 dip)
     }
@@ -27,7 +38,17 @@ class LoginScaloid extends SActivity {
     toast("We're stopped")
   }
 
-  def signin(uname:String, pass:String) = {
-    toast("Sign in " + uname + ", " + pass)
+  def loginPressed(uname:String, pass:String) = {
+    val dlg = ProgressDialog.show(this, null, getString(R.string.busy_login), true)
+
+    Future {
+      val result = performLogin
+      dlg.dismiss()
+    }
+  }
+
+  def performLogin: String = {
+    Thread.sleep(1000)
+    scala.io.Source.fromURL(getString(R.string.dummy_login_url)).mkString
   }
 }
